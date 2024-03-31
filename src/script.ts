f79de8366634ea2e078733b6161dfa82f1490fc5
Lior Bas <look551@gmail.com>
2024-03-31 16:23:02 +0300

    update 2

--- src/script.ts
+++ src/script.ts
@@ -474,15 +474,24 @@ async function collapseButton(
     `#collapseExampleButton${index}`
   ) as HTMLButtonElement;
 
-  let lastFetchTimes: number[] = JSON.parse(
-    localStorage.getItem(`lastFetchTimes`) || "[]"
-  );
+  let lastFetchTimes: number[] = [];
+  const lastFetchTimesFromStorage = localStorage.getItem("lastFetchTimes");
+  if (
+    lastFetchTimesFromStorage &&
+    lastFetchTimesFromStorage.startsWith("[") &&
+    lastFetchTimesFromStorage.endsWith("]")
+  ) {
+    lastFetchTimes = JSON.parse(lastFetchTimesFromStorage);
+  }
+
   const currentTime: number = Date.now();
   const twoMinutesInMilliseconds: number = 2 * 60 * 1000;
 
   if (
     lastFetchTimes.length <= index ||
-    currentTime - lastFetchTimes[index] > twoMinutesInMilliseconds
+    lastFetchTimes[index] === null ||
+    (typeof lastFetchTimes[index] === "number" &&
+      currentTime - lastFetchTimes[index]! > twoMinutesInMilliseconds)
   ) {
     idCoinData = await fetchData(idCoinData, index);
     updateUI(idCoinData, infoContainer, moreInfoButton);
@@ -658,137 +667,3 @@ function handleSaveChangesButton(
 
   updateToggleStatesOnLoad(arrayOfCoins);
 }
-
-window.onload = async function chartCoin() {
-  // Retrieve the coin ID from localStorage
-  const toggleInput = JSON.parse(localStorage.getItem("toggleInput") || "");
-  // Make a fetch request to the API using the coin ID
-  const response = await fetch(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${toggleInput}&tsyms=USD`
-  );
-
-  // Parse the response to get the coin value in USD
-  const data = await response.json();
-  const coinValue = data[toggleInput].USD;
-
-  var chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-    zoomEnabled: true,
-    theme: "dark2",
-    title: {
-      text: "Business Growth From 2000 to 2017",
-    },
-    axisX: {
-      title: "Year",
-      valueFormatString: "####",
-      interval: 2,
-    },
-    axisY: {
-      logarithmic: true, //change it to false
-      title: "Profit in USD (Log)",
-      prefix: "$",
-      titleFontColor: "#6D78AD",
-      lineColor: "#6D78AD",
-      gridThickness: 0,
-      lineThickness: 1,
-      labelFormatter: addSymbols,
-    },
-    axisY2: {
-      title: "Profit in USD",
-      prefix: "$",
-      titleFontColor: "#51CDA0",
-      logarithmic: false, //change it to true
-    },
-    data: [
-      {
-        type: "line",
-        xValueFormatString: "####",
-        yValueFormatString: "$#,##0.##",
-        showInLegend: true,
-        name: "Log Scale",
-        dataPoints: [
-          { x: 2001, y: 8000 },
-          { x: 2002, y: 20000 },
-          { x: 2003, y: 40000 },
-          { x: 2004, y: 60000 },
-          { x: 2005, y: 90000 },
-          { x: 2006, y: 140000 },
-          { x: 2007, y: 200000 },
-          { x: 2008, y: 400000 },
-          { x: 2009, y: 600000 },
-          { x: 2010, y: 800000 },
-          { x: 2011, y: 900000 },
-          { x: 2012, y: 1400000 },
-          { x: 2013, y: 2000000 },
-          { x: 2014, y: 4000000 },
-          { x: 2015, y: 6000000 },
-          { x: 2016, y: 8000000 },
-          { x: 2017, y: 9000000 },
-        ],
-      },
-      {
-        type: "line",
-        xValueFormatString: "####",
-        yValueFormatString: "$#,##0.##",
-        axisYType: "secondary",
-        showInLegend: true,
-        name: "Linear Scale",
-        dataPoints: [
-          { x: 2001, y: 8000 },
-          { x: 2002, y: 20000 },
-          { x: 2003, y: 40000 },
-          { x: 2004, y: 60000 },
-          { x: 2005, y: 90000 },
-          { x: 2006, y: 140000 },
-          { x: 2007, y: 200000 },
-          { x: 2008, y: 400000 },
-          { x: 2009, y: 600000 },
-          { x: 2010, y: 800000 },
-          { x: 2011, y: 900000 },
-          { x: 2012, y: 1400000 },
-          { x: 2013, y: 2000000 },
-          { x: 2014, y: 4000000 },
-          { x: 2015, y: 6000000 },
-          { x: 2016, y: 8000000 },
-          { x: 2017, y: 9000000 },
-        ],
-      },
-    ],
-  });
-  chart.render();
-
-  function addSymbols(e: { value: number }) {
-    var suffixes = ["", "K", "M", "B"];
-
-    var order = Math.max(
-      Math.floor(Math.log(Math.abs(e.value)) / Math.log(1000)),
-      0
-    );
-    if (order > suffixes.length - 1) order = suffixes.length - 1;
-
-    var suffix = suffixes[order];
-    return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
-  }
-};
-
-function handleLiveReportsButton(): void {
-  const liveReportsButton = document.querySelector(
-    `#liveReports`
-  ) as HTMLButtonElement;
-
-  const divCurrencyCards = document.querySelector(`#hideMe`) as HTMLDivElement;
-
-  const divChart = document.querySelector(`#chartContainer`) as HTMLDivElement;
-
-  liveReportsButton.addEventListener(`click`, function () {
-    if (divChart.hidden) {
-      divChart.hidden = false;
-      divCurrencyCards.hidden = true;
-    } else {
-      divChart.hidden = true;
-      divCurrencyCards.hidden = false;
-    }
-  });
-}
-
-handleLiveReportsButton();
