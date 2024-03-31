8c1ebfe530cd4d3ea3164a165203c104de998ce6
Lior Bas <look551@gmail.com>
2024-03-31 14:00:13 +0300

    update 1

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 class CryptoCoinsInfo {
   id: string;
+  symbol: string;
   image: {
     thumb: string;
   };
@@ -13,12 +14,14 @@ class CryptoCoinsInfo {
 
   constructor(
     id: string,
+    symbol: string,
     image: { thumb: string },
     market_data: { current_price: { usd: number; eur: number; ils: number } }
   ) {
     this.image = image;
     this.market_data = market_data;
     this.id = id;
+    this.symbol = symbol;
   }
 }
 
@@ -137,7 +140,18 @@ function createCardElements(
   };
 }
 
-function appendCardElements(elements: any, cardBodyDiv: HTMLDivElement) {
+function appendCardElements(
+  elements: {
+    cardDiv?: HTMLDivElement;
+    cardBodyDiv?: HTMLDivElement;
+    cardTitle: HTMLDivElement;
+    cardText: HTMLDivElement;
+    toggleInput: HTMLDivElement;
+    moreInfoButton: HTMLButtonElement;
+    infoContainer: HTMLDivElement;
+  },
+  cardBodyDiv: HTMLDivElement
+) {
   cardBodyDiv.appendChild(elements.cardTitle);
   cardBodyDiv.appendChild(elements.cardText);
   cardBodyDiv.appendChild(elements.toggleInput);
@@ -146,7 +160,15 @@ function appendCardElements(elements: any, cardBodyDiv: HTMLDivElement) {
 }
 
 function setupCardEvents(
-  elements: any,
+  elements: {
+    cardDiv?: HTMLDivElement;
+    cardBodyDiv?: HTMLDivElement;
+    cardTitle?: HTMLDivElement;
+    cardText?: HTMLDivElement;
+    toggleInput?: HTMLDivElement;
+    moreInfoButton: HTMLButtonElement;
+    infoContainer?: HTMLDivElement;
+  },
   coin: CryptoCoins,
   index: number,
   arrayOfCoins: CryptoCoins[]
@@ -244,8 +266,8 @@ function handleToggleChecked(
   arrayOfCoins: CryptoCoins[],
   toggleArray: string[]
 ): void {
-  if (toggleArray.length < 6 && !toggleArray.includes(coin.id)) {
-    toggleArray.push(coin.id);
+  if (toggleArray.length < 6 && !toggleArray.includes(coin.symbol)) {
+    toggleArray.push(coin.symbol);
     saveToggleArrayToLocalStorage(toggleArray);
     modalBody(arrayOfCoins);
   } else {
@@ -258,7 +280,7 @@ function handleToggleUnchecked(
   arrayOfCoins: CryptoCoins[],
   toggleArray: string[]
 ): void {
-  toggleArray = toggleArray.filter((id) => id !== coin.id);
+  toggleArray = toggleArray.filter((id) => id !== coin.symbol);
   saveToggleArrayToLocalStorage(toggleArray);
   modalBody(arrayOfCoins);
 }
@@ -287,7 +309,7 @@ function updateToggleStatesOnLoad(arrayOfCoins: CryptoCoins[]): void {
       `#toggleInput${index}`
     ) as HTMLInputElement;
 
-    if (toggleArray.includes(coin.id)) {
+    if (toggleArray.includes(coin.symbol)) {
       toggleInput.checked = true;
     } else {
       toggleInput.checked = false;
@@ -530,28 +552,6 @@ function createSpinnerForCollapse(): HTMLDivElement {
   return spinnerDiv;
 }
 
-// function searchCoin(arrayOfCoins: CryptoCoins[]): void {
-//   const searchInput = document.querySelector(`#search`) as HTMLInputElement;
-//   const searchBtn = document.querySelector(`#searchBtn`) as HTMLButtonElement;
-
-//   searchBtn.addEventListener(`click`, () => {
-//     const searchValue = searchInput.value.toLowerCase();
-
-//     const filteredCoins = arrayOfCoins.filter((coin) => {
-//       return (
-//         coin.name.toLowerCase().includes(searchValue) ||
-//         coin.symbol.toLowerCase().includes(searchValue)
-//       );
-//     });
-
-//     const cardRow = document.querySelector(`#card`) as HTMLDivElement;
-
-//     cardRow.innerHTML = ``;
-
-//     createCardInHtml(filteredCoins);
-//   });
-// }
-
 function searchCoin(arrayOfCoins: CryptoCoins[]): void {
   const searchInput = document.querySelector(`#search`) as HTMLInputElement;
   const searchBtn = document.querySelector(`#searchBtn`) as HTMLButtonElement;
@@ -659,123 +659,136 @@ function handleSaveChangesButton(
   updateToggleStatesOnLoad(arrayOfCoins);
 }
 
-// charts
-// window.onload = function () {
-//   var options = {
-//     exportEnabled: true,
-//     animationEnabled: true,
-//     title: {
-//       text: "Units Sold VS Profit",
-//     },
-//     subtitles: [
-//       {
-//         text: "Click Legend to Hide or Unhide Data Series",
-//       },
-//     ],
-//     axisX: {
-//       title: "States",
-//     },
-//     axisY: {
-//       title: "Units Sold",
-//       titleFontColor: "#4F81BC",
-//       lineColor: "#4F81BC",
-//       labelFontColor: "#4F81BC",
-//       tickColor: "#4F81BC",
-//     },
-//     axisY2: {
-//       title: "Profit in USD",
-//       titleFontColor: "#C0504E",
-//       lineColor: "#C0504E",
-//       labelFontColor: "#C0504E",
-//       tickColor: "#C0504E",
-//     },
-//     toolTip: {
-//       shared: true,
-//     },
-//     legend: {
-//       cursor: "pointer",
-//       itemclick: toggleDataSeries,
-//     },
-//     data: [
-//       {
-//         type: "spline",
-//         name: "Units Sold",
-//         showInLegend: true,
-//         xValueFormatString: "MMM YYYY",
-//         yValueFormatString: "#,##0 Units",
-//         dataPoints: [
-//           { x: new Date(2016, 0, 1), y: 120 },
-//           { x: new Date(2016, 1, 1), y: 135 },
-//           { x: new Date(2016, 2, 1), y: 144 },
-//           { x: new Date(2016, 3, 1), y: 103 },
-//           { x: new Date(2016, 4, 1), y: 93 },
-//           { x: new Date(2016, 5, 1), y: 129 },
-//           { x: new Date(2016, 6, 1), y: 143 },
-//           { x: new Date(2016, 7, 1), y: 156 },
-//           { x: new Date(2016, 8, 1), y: 122 },
-//           { x: new Date(2016, 9, 1), y: 106 },
-//           { x: new Date(2016, 10, 1), y: 137 },
-//           { x: new Date(2016, 11, 1), y: 142 },
-//         ],
-//       },
-//       {
-//         type: "spline",
-//         name: "Profit",
-//         axisYType: "secondary",
-//         showInLegend: true,
-//         xValueFormatString: "MMM YYYY",
-//         yValueFormatString: "$#,##0.#",
-//         dataPoints: [
-//           { x: new Date(2016, 0, 1), y: 19034.5 },
-//           { x: new Date(2016, 1, 1), y: 20015 },
-//           { x: new Date(2016, 2, 1), y: 27342 },
-//           { x: new Date(2016, 3, 1), y: 20088 },
-//           { x: new Date(2016, 4, 1), y: 20234 },
-//           { x: new Date(2016, 5, 1), y: 29034 },
-//           { x: new Date(2016, 6, 1), y: 30487 },
-//           { x: new Date(2016, 7, 1), y: 32523 },
-//           { x: new Date(2016, 8, 1), y: 20234 },
-//           { x: new Date(2016, 9, 1), y: 27234 },
-//           { x: new Date(2016, 10, 1), y: 33548 },
-//           { x: new Date(2016, 11, 1), y: 32534 },
-//         ],
-//       },
-//     ],
-//   };
+window.onload = async function chartCoin() {
+  // Retrieve the coin ID from localStorage
+  const toggleInput = JSON.parse(localStorage.getItem("toggleInput") || "");
+  // Make a fetch request to the API using the coin ID
+  const response = await fetch(
+    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${toggleInput}&tsyms=USD`
+  );
 
-//   ($("#chartContainer") as any).CanvasJSChart(options);
+  // Parse the response to get the coin value in USD
+  const data = await response.json();
+  const coinValue = data[toggleInput].USD;
 
-//   function toggleDataSeries(e: any) {
-//     if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
-//       e.dataSeries.visible = false;
-//     } else {
-//       e.dataSeries.visible = true;
-//     }
-//     e.chart.render();
-//   }
-// };
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    zoomEnabled: true,
+    theme: "dark2",
+    title: {
+      text: "Business Growth From 2000 to 2017",
+    },
+    axisX: {
+      title: "Year",
+      valueFormatString: "####",
+      interval: 2,
+    },
+    axisY: {
+      logarithmic: true, //change it to false
+      title: "Profit in USD (Log)",
+      prefix: "$",
+      titleFontColor: "#6D78AD",
+      lineColor: "#6D78AD",
+      gridThickness: 0,
+      lineThickness: 1,
+      labelFormatter: addSymbols,
+    },
+    axisY2: {
+      title: "Profit in USD",
+      prefix: "$",
+      titleFontColor: "#51CDA0",
+      logarithmic: false, //change it to true
+    },
+    data: [
+      {
+        type: "line",
+        xValueFormatString: "####",
+        yValueFormatString: "$#,##0.##",
+        showInLegend: true,
+        name: "Log Scale",
+        dataPoints: [
+          { x: 2001, y: 8000 },
+          { x: 2002, y: 20000 },
+          { x: 2003, y: 40000 },
+          { x: 2004, y: 60000 },
+          { x: 2005, y: 90000 },
+          { x: 2006, y: 140000 },
+          { x: 2007, y: 200000 },
+          { x: 2008, y: 400000 },
+          { x: 2009, y: 600000 },
+          { x: 2010, y: 800000 },
+          { x: 2011, y: 900000 },
+          { x: 2012, y: 1400000 },
+          { x: 2013, y: 2000000 },
+          { x: 2014, y: 4000000 },
+          { x: 2015, y: 6000000 },
+          { x: 2016, y: 8000000 },
+          { x: 2017, y: 9000000 },
+        ],
+      },
+      {
+        type: "line",
+        xValueFormatString: "####",
+        yValueFormatString: "$#,##0.##",
+        axisYType: "secondary",
+        showInLegend: true,
+        name: "Linear Scale",
+        dataPoints: [
+          { x: 2001, y: 8000 },
+          { x: 2002, y: 20000 },
+          { x: 2003, y: 40000 },
+          { x: 2004, y: 60000 },
+          { x: 2005, y: 90000 },
+          { x: 2006, y: 140000 },
+          { x: 2007, y: 200000 },
+          { x: 2008, y: 400000 },
+          { x: 2009, y: 600000 },
+          { x: 2010, y: 800000 },
+          { x: 2011, y: 900000 },
+          { x: 2012, y: 1400000 },
+          { x: 2013, y: 2000000 },
+          { x: 2014, y: 4000000 },
+          { x: 2015, y: 6000000 },
+          { x: 2016, y: 8000000 },
+          { x: 2017, y: 9000000 },
+        ],
+      },
+    ],
+  });
+  chart.render();
 
-// function hiddenHomePageForLiveReports() {
-//   const charts = document.querySelector(`.charts`) as HTMLDivElement;
-//   const liveReportsBtn = document.querySelector(
-//     `#liveReports`
-//   ) as HTMLInputElement;
-//   const homePage = document.querySelector(`.hideMe`) as HTMLDivElement;
+  function addSymbols(e: { value: number }) {
+    var suffixes = ["", "K", "M", "B"];
 
-//   liveReportsBtn.addEventListener("click", function () {
-//     homePage.setAttribute("hidden", "true");
-//     charts.removeAttribute("hidden");
-//   });
-// }
+    var order = Math.max(
+      Math.floor(Math.log(Math.abs(e.value)) / Math.log(1000)),
+      0
+    );
+    if (order > suffixes.length - 1) order = suffixes.length - 1;
 
-// hiddenHomePageForLiveReports();
+    var suffix = suffixes[order];
+    return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+  }
+};
 
-// async function fetchForCharts(
-//   arrayOfCoins: CryptoCoins[],
-//   toggleArray: string[]
-// ) {
-//   const response = await fetch(
-//     `https://min-api.cryptocompare.com/data/pricemulti?fsyms=$&tsyms=USD`
-//   );
-//   const data = await response.json();
-// }
+function handleLiveReportsButton(): void {
+  const liveReportsButton = document.querySelector(
+    `#liveReports`
+  ) as HTMLButtonElement;
+
+  const divCurrencyCards = document.querySelector(`#hideMe`) as HTMLDivElement;
+
+  const divChart = document.querySelector(`#chartContainer`) as HTMLDivElement;
+
+  liveReportsButton.addEventListener(`click`, function () {
+    if (divChart.hidden) {
+      divChart.hidden = false;
+      divCurrencyCards.hidden = true;
+    } else {
+      divChart.hidden = true;
+      divCurrencyCards.hidden = false;
+    }
+  });
+}
+
+handleLiveReportsButton();
